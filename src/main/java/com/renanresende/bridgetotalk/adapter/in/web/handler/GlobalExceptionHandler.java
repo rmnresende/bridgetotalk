d816db6b877bded310188be27a2d2232010087ca
Renan Resende <rmnresende@gmail.com>
2025-11-30 00:26:57 -0300

    feat: Add Constraints resolver and improve  constraint erros handling

--- src/main/java/com/renanresende/bridgetotalk/adapter/in/web/handler/GlobalExceptionHandler.java
+++ src/main/java/com/renanresende/bridgetotalk/adapter/in/web/handler/GlobalExceptionHandler.java
@@ -1,26 +1,33 @@
 package com.renanresende.bridgetotalk.adapter.in.web.handler;
 
 import com.renanresende.bridgetotalk.adapter.in.web.dto.ApiError;
+import com.renanresende.bridgetotalk.adapter.in.web.resolver.ConstraintMessageResolver;
 import com.renanresende.bridgetotalk.domain.exception.BusinessException;
 import com.renanresende.bridgetotalk.domain.exception.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
-import java.util.NoSuchElementException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final ConstraintMessageResolver constraintMessageResolver;
+
+    public GlobalExceptionHandler(ConstraintMessageResolver constraintMessageResolver) {
+        this.constraintMessageResolver = constraintMessageResolver;
+    }
+
+    // 422 - Business Rules
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiError> handleBusinessException(
             BusinessException ex,
             HttpServletRequest request
     ) {
-
         var error = new ApiError(
                 HttpStatus.UNPROCESSABLE_ENTITY.value(),
                 "BUSINESS_ERROR",
@@ -32,6 +39,7 @@ public class GlobalExceptionHandler {
         return ResponseEntity.unprocessableEntity().body(error);
     }
 
+    // 404 - Not Found
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleNotFound(
             ResourceNotFoundException ex,
@@ -48,6 +56,27 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 
+    // 409 - Constraint violation
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ApiError> handleConstraintViolation(
+            DataIntegrityViolationException ex,
+            HttpServletRequest request
+    ) {
+        String dbMessage = ex.getMostSpecificCause().getMessage();
+        String friendlyMessage = constraintMessageResolver.resolve(dbMessage);
+
+        var error = new ApiError(
+                HttpStatus.CONFLICT.value(),
+                "CONFLICT",
+                friendlyMessage,
+                request.getRequestURI(),
+                Instant.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
+
+    // 500 - generic error
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handleGeneric(
             Exception ex,
--- src/main/java/com/renanresende/bridgetotalk/config/ConstraintMessagesProperties.java
+++ src/main/java/com/renanresende/bridgetotalk/config/ConstraintMessagesProperties.java
@@ -1,4 +1,30 @@
 package com.renanresende.bridgetotalk.config;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@ConfigurationProperties(prefix = "app.constraints")
+@PropertySource(value = "classpath:constraints-messages.yml", factory = YamlPropertySourceFactory.class)
 public class ConstraintMessagesProperties {
+
+    private Map<String, String> messages;
+
+    public Map<String, String> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Map<String, String> messages) {
+        this.messages = messages;
+    }
+
+    @PostConstruct
+    public void test() {
+        System.out.println("Loaded: " + messages);
+    }
 }
